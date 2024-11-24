5a63854b9a1b0cedbbe160d4722ed1e139981360
Ambroise Bisiaux <bisiauxambroise@gmail.com>
2024-11-24 22:00:51 +0100

    quand on utilise l'ia c'est quand même plus facile

--- Items/item_manager.gd
+++ Items/item_manager.gd
@@ -23,9 +23,8 @@ var mountain_atlas = Vector2i(18,4)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	setup()
-	SpawnItemWorld(itemPrototypes["Trees"][1],0.1)
 	SpawnItemWorld(itemPrototypes["Rocks"][1],0.1)
-	SpawnForest(50)
+	SpawnForest(0.05)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -83,15 +82,36 @@ func SpawnItemWorld(item, odd):
 				SpawnItem(item, Vector2(pos))
 				
 func SpawnForest(odd):
-	for i in range(-width+5, width-5):
-		for j in range(-height+5, height-5):
-			var pos = Vector2(i, j)
-			if typeof(itemsInWorld[i][j]) != TYPE_INT and rng.randf_range(0,100) <= odd and IsItemInCategory(itemsInWorld[i][j], 1):
-				while odd != 0:
-					for x in range(4):
-						SpawnItem(itemPrototypes[rng.randi_range(0,3)], Vector2(pos))
+	# Taille des zones denses et clairsemées
+	var cluster_size = rng.randi_range(5, 10)  # Taille des clusters denses (en tuiles)
+	var sparse_size = rng.randi_range(10, 20)  # Taille des zones sans arbres (en tuiles)
+	# Itération sur la carte
+	for i in range(-width + 5, width - 5, sparse_size):
+		for j in range(-height + 5, height - 5, sparse_size):
+			# Décider aléatoirement si cette zone sera dense ou claire
+			if rng.randf() < 0.2:  # 20% de chances d'avoir une zone dense
+				SpawnTreeCluster(Vector2i(i, j), cluster_size, odd * 2)  # Densité augmentée
+			else:
+				SpawnTreeCluster(Vector2i(i, j), cluster_size, odd * 0.1)  # Densité réduite
 
 
+func SpawnTreeCluster(center: Vector2i, size: int, odd: float):
+	# Crée un cluster d'arbres autour d'un point central
+	for i in range(center.x - size, center.x + size):
+		for j in range(center.y - size, center.y + size):
+			# Vérifie si la position est valide
+			if -width + 5 <= i and i < width - 5 and -height + 5 <= j and j < height - 5:
+				var pos = Vector2(i, j)
+				var tile_coords = tilemap.get_cell_atlas_coords(pos)
+				# Place un arbre avec une probabilité donnée
+				if tile_coords in base_grass_atlas and rng.randf() < odd and typeof(itemsInWorld[i][j]) == TYPE_INT:
+					var tree_prototype = null
+					if itemPrototypes.has("Trees") and itemPrototypes["Trees"].size() > 0:
+						tree_prototype = itemPrototypes["Trees"][rng.randi_range(0,3)]
+					if tree_prototype:
+						SpawnItem(tree_prototype, Vector2i(i, j))
+
+	
 
 func FindNearestItem(itemCategory: ItemCategory, worldPos: Vector2):
 	if len(itemsInWorld) == 0:
